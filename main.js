d2cefb8004d0f7acab570a08fc9ea09bf0c5a829
Luciano Boldrini <luchoboldrini22@gmail.com>
2023-08-10 16:12:27 -0300

    Segunda Preentrega mejorada

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Creamos una variable  llamada "colchones" que sea un array de objetos.
-let colchones = [
+const colchones = [
     {
         tipo: "Espuma De Alta Densidad",
         medida: "1 plaza",
@@ -81,45 +81,73 @@ let colchones = [
         marca: "Maxiking"
     }
 ];
+// Función para mostrar los detalles de un colchón
+function mostrarColchon(colchon) {
+    console.log(`Tipo: ${colchon.tipo}`);
+    console.log(`Medida: ${colchon.medida}`);
+    console.log(`Precio: ${colchon.precio}`);
+    console.log(`Marca: ${colchon.marca}`);
+    console.log("--------------------");
+}
 
-// Creamos una función llamada "mostrarColchones" que reciba como parámetro un array de colchones y los muestre en la pantalla
-function mostrarColchones(arrayDeColchones) {
-    // Recorremos el array con un bucle for
-    for (let i = 0; i < arrayDeColchones.length; i++) {
-        // Obtenemos el colchón actual del array
-        let colchon = arrayDeColchones[i];
-        // Mostramos sus propiedades en la pantalla, console.log
-        console.log(`Tipo: ${colchon.tipo}`);
-        console.log(`Medida: ${colchon.medida}`);
-        console.log(`Precio: ${colchon.precio}`);
-        console.log(`Marca: ${colchon.marca}`);
-        console.log("--------------------");
+// Función para filtrar colchones por medida
+function filtrarColchonesPorMedida(medida) {
+    // Convertir la medida ingresada a minúsculas
+    const medidaLowerCase = medida.toLowerCase();
+
+    // Medidas válidas para filtrar
+    const medidasValidas = ["1 plaza", "1 plaza y media", "2 plazas", "queen", "king"];
+    if (medidasValidas.includes(medidaLowerCase)) {
+        // Utiliza el método filter para obtener colchones que coincidan con la medida
+        return colchones.filter(colchon => colchon.medida.toLowerCase() === medidaLowerCase);
+    } else {
+        console.log("Medida no válida.");
+        return [];
     }
 }
 
-// Creamos una función llamada "filtrarColchones" que reciba como parámetro una medida y devuelva un nuevo array de colchones que solo contenga los que tengan esa medida
-function filtrarColchones(medida) {
-    // Declaramos una variable  llamada "colchonesFiltrados" que sea un array vacío
-    let colchonesFiltrados = [];
-    // Recorremos el array de colchones con un bucle for
-    for (let i = 0; i < colchones.length; i++) {
-        // Obtenemos el colchón actual del array
-        let colchon = colchones[i];
-        // Comparamos si la medida del colchón es igual a la medida que recibimos como parámetro
-        if (colchon.medida === medida) {
-            // Si es igual, agregamos el colchón al array de colchonesFiltrados usando el método push
-            colchonesFiltrados.push(colchon);
-        }
+// Función para mostrar todos los colchones filtrados
+function mostrarColchonesFiltrados(colchonesFiltrados) {
+    if (colchonesFiltrados.length > 0) {
+        // Utiliza la función mostrarColchon para cada colchón filtrado
+        colchonesFiltrados.forEach(mostrarColchon);
+    } else {
+        console.log("No se encontraron colchones con esa medida.");
     }
-    // Devolvemos el valor de colchonesFiltrados
-    return colchonesFiltrados;
 }
 
-// Pedimos al usuario que ingrese la medida que desea filtrar y guardamos su respuesta en una variable de JS llamada medida
-let medida = prompt("Ingrese la medida que desea Buscar");
+// Solicitar al usuario ingresar la medida que desea buscar
+let medida = prompt("Ingrese la medida que desea buscar (1 plaza, 1 plaza y media, 2 plazas, queen, king)");
 
-// Llamamos a la función "filtrarColchones" pasándole la variable medida y guardamos el resultado en una variable de JS llamada colchonesFiltrados
-let colchonesFiltrados = filtrarColchones(medida);
+// Convertir la entrada del usuario a minúsculas
+medida = medida.toLowerCase();
 
-// Llamamos a la función "mostrarColchones" pasándole la variable "colchonesFiltrados"
-mostrarColchones(colchonesFiltrados);
+if (medida) {
+    // Filtrar los colchones por la medida ingresada
+    const colchonesFiltrados = filtrarColchonesPorMedida(medida);
+
+    if (colchonesFiltrados.length > 0) {
+        // Crear un listado de colchones en la medida elegida
+        const listado = colchonesFiltrados.reduce((acumulador, colchon) => {
+            acumulador += `Tipo: ${colchon.tipo}\n`;
+            acumulador += `Medida: ${colchon.medida}\n`;
+            acumulador += `Precio: ${colchon.precio}\n`;
+            acumulador += `Marca: ${colchon.marca}\n`;
+            acumulador += "--------------------\n";
+            return acumulador;
+        }, "");
+        
+        
+
+        // Mostrar el listado en la consola y en un alert
+        console.log(listado);
+        alert(listado);
+    } else {
+        console.log("No se encontraron colchones con esa medida.");
+        alert("No se encontraron colchones con esa medida.");
+    }
+} else {
+    console.log("No se ingresó ninguna medida.");
+    alert("No se ingresó ninguna medida.");
+    
+}
